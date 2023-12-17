0270efbcb64bbb4cf4c0aa22b45370439e39a486
itzila <psupodhvost@gmail.com>
2023-12-17 19:43:53 +0700

    Модулироване данных

---
+++ elements/sidebar.php
@@ -0,0 +1,15 @@
+<div class="col-lg-4">
+    <div class="blog_right_sidebar">
+        <?php require_once("./aside/search_widget.php") ?>
+
+        <?php require_once("./aside/post_category_widget.php") ?>
+
+        <?php require_once("./aside/popular_post_widget.php") ?>
+
+        <?php require_once("./aside/tag_cloud_widget.php") ?>
+
+        <?php require_once("./aside/instagram_feeds.php") ?>
+
+        <?php require_once("./aside/newsletter_widget.php") ?>
+    </div>
+</div>
---
+++ latest_news.php
@@ -0,0 +1,163 @@
+<?php require_once ("./elements/header.php"); ?>
+
+    <main>
+        <!-- Start Youtube -->
+        <div class="youtube-area">
+            <div class="container">
+                <!-- Hot Aimated News Tittle-->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="trending-tittle">
+                            <strong>Trending now</strong>
+                            <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
+                            <div class="trending-animated">
+                                <ul id="js-news" class="js-hidden">
+                                    <li class="news-item">Bangladesh dolor sit amet, consectetur adipisicing elit.</li>
+                                    <li class="news-item">Spondon IT sit amet, consectetur.......</li>
+                                    <li class="news-item">Rem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="video-items-active">
+                            <div class="video-items text-center">
+                                <iframe src="https://www.youtube.com/embed/CicQIuG8hBo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="video-items text-center">
+                                <iframe  src="https://www.youtube.com/embed/rIz00N40bag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="video-items text-center">
+                                <iframe src="https://www.youtube.com/embed/CONfhrASy44" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="video-items text-center">
+                                <iframe src="https://www.youtube.com/embed/lq6fL2ROWf8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="video-items text-center">
+                                <iframe src="https://www.youtube.com/embed/0VxlQlacWV4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="video-info">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="video-caption">
+                                <div class="top-caption">
+                                    <span class="color1">Politics</span>
+                                </div>
+                                <div class="bottom-caption">
+                                    <h2>Welcome To The Best Model Winner Contest At Look of the year</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod ipsum dolor sit. Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod ipsum dolor sit. Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod ipsum dolor sit lorem ipsum dolor sit.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="testmonial-nav text-center">
+                                <div class="single-video">
+                                    <iframe  src="https://www.youtube.com/embed/CicQIuG8hBo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <div class="video-intro">
+                                        <h4>Welcotme To The Best Model Winner Contest</h4>
+                                    </div>
+                                </div>
+                                <div class="single-video">
+                                    <iframe  src="https://www.youtube.com/embed/rIz00N40bag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <div class="video-intro">
+                                        <h4>Welcotme To The Best Model Winner Contest</h4>
+                                    </div>
+                                </div>
+                                <div class="single-video">
+                                    <iframe src="https://www.youtube.com/embed/CONfhrASy44" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <div class="video-intro">
+                                        <h4>Welcotme To The Best Model Winner Contest</h4>
+                                    </div>
+                                </div>
+                                <div class="single-video">
+                                    <iframe src="https://www.youtube.com/embed/lq6fL2ROWf8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <div class="video-intro">
+                                        <h4>Welcotme To The Best Model Winner Contest</h4>
+                                    </div>
+                                </div>
+                                <div class="single-video">
+                                    <iframe src="https://www.youtube.com/embed/0VxlQlacWV4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <div class="video-intro">
+                                        <h4>Welcotme To The Best Model Winner Contest</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+         <!-- End Start youtube -->
+        <!-- About US Start -->
+        <div class="about-area">
+            <div class="container">
+                    <!-- Hot Aimated News Tittle-->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="trending-tittle">
+                                <strong>Trending now</strong>
+                                <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
+                                <div class="trending-animated">
+                                    <ul id="js-news" class="js-hidden">
+                                        <li class="news-item">Bangladesh dolor sit amet, consectetur adipisicing elit.</li>
+                                        <li class="news-item">Spondon IT sit amet, consectetur.......</li>
+                                        <li class="news-item">Rem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                                    </ul>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                   <div class="row">
+                        <div class="col-lg-8">
+                            <!-- Trending Tittle -->
+                            <div class="about-right mb-90">
+                                <div class="about-img">
+                                    <img src="assets/img/trending/trending_top.jpg" alt="">
+                                </div>
+                                <div class="section-tittle mb-30 pt-30">
+                                    <h3>Here come the moms in space</h3>
+                                </div>
+                                <div class="about-prea">
+                                    <p class="about-pera1 mb-25">Moms are like…buttons? Moms are like glue. Moms are like pizza crusts. Moms are the ones who make sure things happen—from birth to school lunch.</p>
+                                    <p class="about-pera1 mb-25">Moms are like…buttons? Moms are like glue. Moms are like pizza crusts. Moms are the ones who make sure things happen—from birth to school lunch.</p>
+                                    <p class="about-pera1 mb-25">
+                                        My hero when I was a kid was my mom. Same for everyone I knew. Moms are untouchable. They’re elegant, smart, beautiful, kind…everything we want to be. At 29 years old, my favorite compliment is being told that I look like my mom. Seeing myself in her image, like this daughter up top, makes me so proud of how far I’ve come, and so thankful for where I come from.
+                                        the refractor telescope uses a convex lens to focus the light on the eyepiece.
+                                        The reflector telescope has a concave lens which means it telescope sits on. The mount is the actual tripod and the wedge is the device that lets you attach the telescope to the mount.
+                                        Moms are like…buttons? Moms are like glue. Moms are like pizza crusts. Moms are the ones who make sure things happen—from birth to school lunch.</p>
+                                </div> 
+                                <div class="section-tittle">
+                                    <h3>Unordered list style?</h3>
+                                </div>
+                                <div class="social-share pt-30">
+                                    <div class="section-tittle">
+                                        <h3 class="mr-20">Share:</h3>
+                                        <ul>
+                                            <li><a href="#"><img src="assets/img/news/icon-ins.png" alt=""></a></li>
+                                            <li><a href="#"><img src="assets/img/news/icon-fb.png" alt=""></a></li>
+                                            <li><a href="#"><img src="assets/img/news/icon-tw.png" alt=""></a></li>
+                                            <li><a href="#"><img src="assets/img/news/icon-yo.png" alt=""></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- From -->
+                            <div class="row">
+                                <?php require_once ("./elements/contact_form.php")?>
+                            </div>
+                        </div>
+                       <?php require_once("./elements/follow_us.php") ?>
+                   </div>
+            </div>
+        </div>
+        <!-- About US End -->
+    </main>
+
+<?php require_once ("./elements/footer.php")?>
